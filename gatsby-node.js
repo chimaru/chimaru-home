2c7dc23871e42e257a845718ee0d76afae5c0188
chimaru <chimarurnrmr@gmail.com>
2021-05-26 00:24:54 +0900

    add page

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,52 +7,54 @@
 // You can delete this file if you're not using it
 
 const path = require('path')
-const { createFilePath } = require('gatsby-source-filesystem');
+const { createFilePath } = require('gatsby-source-filesystem')
 
-exports.onCreateNode = ({node, getNode, actions}) => {
-    if (node.internal.type === 'MarkdownRemark') {
-        const {createNodeField} = actions;
-        const relativeFilePath = createFilePath ({
-            node,
-            getNode,
-            bathPath: 'posts',
-            trailingSlash: false
-        });
-        createNodeField({
-            node,
-            name: 'slug',
-            value: `/posts${relativeFilePath}`
-        })
-    }
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  if (node.internal.type === 'MarkdownRemark') {
+    const { createNodeField } = actions
+    const relativeFilePath = createFilePath({
+      node,
+      getNode,
+      bathPath: 'posts',
+      trailingSlash: false,
+    })
+    createNodeField({
+      node,
+      name: 'slug',
+      value: `/posts${relativeFilePath}`,
+    })
+  }
 }
 
 const createPostPages = (edges, createPage) => {
-    const component = path.resolve('./src/templates/post.js');
-    edges.forEach(({node}) => {
-        const { slug } = node.fields;
-        createPage({
-            component,
-            path: slug,
-            context: {
-                slug
-            }
-        })
+  const component = path.resolve('./src/templates/post.js')
+  edges.forEach(({ node }) => {
+    const { slug } = node.fields
+    createPage({
+      component,
+      path: slug,
+      context: {
+        slug,
+      },
     })
+  })
 }
 
 exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions;
-    const result = await graphql(`{
-        allMarkdownRemark {
-            edges {
-                node {
-                    fields {
-                        slug
-                    }
-                }
+  const { createPage } = actions
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
             }
+          }
         }
-    }`);
-    const { edges } = result.data.allMarkdownRemark;
-    createPostPages(edges, createPage);
+      }
+    }
+  `)
+  const { edges } = result.data.allMarkdownRemark
+  createPostPages(edges, createPage)
 }
